96840bfac1c6bae97cbd99265d795192dc910a13
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-08-05 22:45:57 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import process from 'process';
-window.process = process;
+window.process = process; // Make process available globally
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
