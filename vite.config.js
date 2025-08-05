e7fe1c7690f1a2c4b5f42e38f15dac454c0ff298
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-08-05 22:43:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Fix for `process is not defined`
+// Fix for process is not defined
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      process: 'process/browser', // use process polyfill
-    },
-  },
   define: {
-    'process.env': {}, // prevent undefined env
+    global: {}, // for some polyfills
+    'process.env': {}, // fake env object
   },
 })
