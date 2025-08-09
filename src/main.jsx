0919ae747677657f0cab93b0e7f1c8daf7166e2c
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-08-09 22:55:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-// Import CSS if you have one
-
-// Ensure errors are logged
-window.addEventListener("error", (e) => {
-  console.error("Global Error:", e.error);
-});
-window.addEventListener("unhandledrejection", (e) => {
-  console.error("Unhandled Promise Rejection:", e.reason);
-});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1 style={{ color: "red" }}>It works ✅</h1>
 );
