3d00a82dca17cb715231ee6b852072139e51daa1
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-08-09 23:01:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify",
-      stream: "stream-browserify",
-      process: "process/browser",
-    },
-  },
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
       define: {
-        global: "globalThis",
+        global: 'globalThis',
       },
-      // Enable esbuild polyfill plugins
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          buffer: true,
           process: true,
+          buffer: true,
         }),
-        NodeModulesPolyfillPlugin(),
+        NodeModulesPolyfillPlugin()
       ],
     },
   },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      stream: 'stream-browserify',
+      crypto: 'crypto-browserify',
     },
   },
-});
+})
